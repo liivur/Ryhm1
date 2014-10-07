4c722d628ceb5ff381dc622157f78d892e2fc7ef
liivur <sander.sats@gmail.com>
2014-10-07 14:42:32 +0300

    Logi confi faili test

--- POS/src/ee/ut/math/tvt/ryhm1/Intro.java
+++ POS/src/ee/ut/math/tvt/ryhm1/Intro.java
@@ -1,18 +1,28 @@
 package ee.ut.math.tvt.ryhm1;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
 
 
 public class Intro {
 	//loogika paika
 	private static final Logger log = LogManager.getLogger(Intro.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		//Kasutame faili log4j.properties
 		
 		//TODO: Fix configuration for the log4j
+//		InputStream logConfigurationFile =  new FileInputStream(new File("etc/log4j2.xml"));
+//		ConfigurationSource source = new ConfigurationSource(logConfigurationFile);
+//		Configurator.initialize(null, source);
 		//PropertyConfigurator.configure("log4j.properties");
 		log.info("Intro initialized");
 		
