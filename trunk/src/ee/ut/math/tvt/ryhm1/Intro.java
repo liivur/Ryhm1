f11dc425fd9dc41d06ca93f79e10d05398403d5e
Erik <erik.r2ni@gmail.com>
2014-10-25 16:30:30 +0300

    Intro klass nii nagu vaja

--- trunk/src/ee/ut/math/tvt/ryhm1/Intro.java
+++ trunk/src/ee/ut/math/tvt/ryhm1/Intro.java
@@ -3,19 +3,44 @@ package ee.ut.math.tvt.ryhm1;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 
 public class Intro {
-	//loogika paika
+
+	// loogika paika
 	private static final Logger log = LogManager.getLogger(Intro.class);
+	private static final String MODE = "console";
 
 	public static void main(String[] args) {
-		
-		log.info("Intro initialized");
-		
-		//Loome uue IntroUI objekti ning muudame selle n�htavaks
-		IntroUI theTeamInfo = new IntroUI("Team Introduction");
-		theTeamInfo.setVisible(true);
-		theTeamInfo.setAlwaysOnTop(true);
-	}
 
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+		log.info("Intro initialized");
+
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			// Loome uue IntroUI objekti ning muudame selle n�htavaks
+			IntroUI introUI = new IntroUI("Team Introduction");
+			introUI.setVisible(true);
+			introUI.setAlwaysOnTop(true);
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+
+			introUI.setAlwaysOnTop(false);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			introUI.setVisible(false);
+		}
+	}
 }
