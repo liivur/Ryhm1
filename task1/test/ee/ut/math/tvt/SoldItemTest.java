ddcbbadd2521efba40aef4cbdf86a25bc58fb007
githubERIK <erik.r2ni@gmail.com>
2014-11-22 12:58:24 +0200

    testGetSum()+testGetSumWithZeroQuantity() COMBO @Before'i ei olnud vaja.

--- task1/test/ee/ut/math/tvt/SoldItemTest.java
+++ task1/test/ee/ut/math/tvt/SoldItemTest.java
@@ -2,6 +2,7 @@ package ee.ut.math.tvt;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,29 +11,38 @@ import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 
 public class SoldItemTest {
 
-	private StockItem stock1;
-	private Long id1 = new Long(111);
-	private String name1 = "sander3";
+	// Testandmed testGetSum() jaoks. NB! ALATI
+	// KONTROLLIDA, ET ANDMEBAAS T÷÷TAKS
+	private SoldItem dealMade;
+	private Long id1 = new Long(500);
+	private String name1 = "sander6";
 	private String desc1 = "testtest";
 	private double price1 = 20.0E0;
 	private int quantity1 = 9;
-	private int quantity2 = 5;
+	private int quantityControl = 5;
 
-	@Before
-	public void setUp() {
-		stock1 = new StockItem(id1, name1, desc1, price1, quantity1);
-	}
+	// Testandmed testGetSumWithZeroQuantity() jaoks. NB! ALATI
+	// KONTROLLIDA, ET ANDMEBAAS T÷÷TAKS
+	private SoldItem dealZero;
+	private Long id2 = new Long(4);
+	private String name2 = "soft drink";
+	private String desc2 = "Sprite";
+	private double price2 = 15.0E0;
+	private int quantity3 = 6;
 
 	@Test
 	public void testGetSum() {
-
-		SoldItem dealMade = new SoldItem (stock1, quantity2);
-		assertEquals(dealMade.getSum(), price1 * quantity2, 0.001);
+		dealMade = new SoldItem(new StockItem(id1, name1, desc1, price1,
+				quantity1), quantityControl);
+		assertEquals(dealMade.getSum(), price1 * quantityControl, 0.0001);
 
 	}
 
-//	@Test
-//	public void testGetSumWithZeroQuantity() {
-//
-//	}
+	@Test
+	public void testGetSumWithZeroQuantity() {
+		dealZero = new SoldItem(new StockItem(id2, name2, desc2, price2,
+				quantity3), 0);
+		assertEquals(dealZero.getSum(), price2 * 0, 0.0001);
+
+	}
 }
